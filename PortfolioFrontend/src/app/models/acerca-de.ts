d84c0abe6641a7e37073872678a7b09387577c0f
Nicolas Helman <nicolashelman11@gmail.com>
2022-05-30 22:49:02 -0300

    Model AcercaDe modified

--- PortfolioFrontend/src/app/models/acerca-de.ts
+++ PortfolioFrontend/src/app/models/acerca-de.ts
@@ -6,19 +6,14 @@ export class AcercaDe {
     ciudad: string;
     telefono: string;
     mail: string;
-    github: string;
-    linkedIn: string;
 
-    constructor(id: number, descripcion: string, fechaNacimiento: string, ciudad: string, telefono: string, mail: string,
-        github: string, linkedIn: string) {
+    constructor(id: number, descripcion: string, fechaNacimiento: string, ciudad: string, telefono: string, mail: string) {
             this.id = id;
             this.descripcion = descripcion;
             this.fechaNacimiento = fechaNacimiento;
             this.ciudad = ciudad;
             this.telefono = telefono;
             this.mail = mail;
-            this.github = github;
-            this.linkedIn = linkedIn;
     }
 
 }
