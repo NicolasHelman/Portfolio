b96bfe028a2da57642bf3300223d547ab547a9a2
Nicolas Helman <nicolashelman11@gmail.com>
2022-07-06 01:45:03 -0300

    Dtos Security added

---
+++ PortfolioBackend/src/main/java/com/PortfolioBackend/security/dtos/JwtDto.java
@@ -0,0 +1,51 @@
+package com.PortfolioBackend.security.dtos;
+
+import org.springframework.security.core.GrantedAuthority;
+import java.util.Collection;
+
+// esta clase la vamos a utilizar cuando hagamos un login
+// nos va a devolver el responseEntity del controlador, un Json Web Token
+public class JwtDto {
+    private String token;
+    private String bearer = "Bearer";
+    private String nombreUsuario;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public JwtDto(String token, String nombreUsuario, Collection<? extends GrantedAuthority> authorities) {
+        this.token = token;
+        this.nombreUsuario = nombreUsuario;
+        this.authorities = authorities;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getBearer() {
+        return bearer;
+    }
+
+    public void setBearer(String bearer) {
+        this.bearer = bearer;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+}
---
+++ PortfolioBackend/src/main/java/com/PortfolioBackend/security/dtos/NuevoUsuario.java
@@ -0,0 +1,60 @@
+package com.PortfolioBackend.security.dtos;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
+
+// esta clase va a mandar un nuevo usuario
+public class NuevoUsuario {
+    @NotBlank
+    private String nombre;
+    @NotBlank
+    private String nombreUsuario;
+    @Email
+    private String email;
+    @NotBlank
+    private String password;
+    // utilizamos esto pq al utilizar una apiRest vamos a usar json, por eso es mejor utilizarlos en cadenas (el trafico va a ser mas agil)
+    private Set<String> roles = new HashSet<>();
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+}
