afe62fb987e03ac84368d7a73cd3c9fbe6bbaff2
Nicolas Helman <nicolashelman11@gmail.com>
2022-07-07 02:36:56 -0300

    JwtDto modified

--- PortfolioBackend/src/main/java/com/PortfolioBackend/security/dtos/JwtDto.java
+++ PortfolioBackend/src/main/java/com/PortfolioBackend/security/dtos/JwtDto.java
@@ -1,20 +1,16 @@
 package com.PortfolioBackend.security.dtos;
 
-import org.springframework.security.core.GrantedAuthority;
-import java.util.Collection;
-
 // esta clase la vamos a utilizar cuando hagamos un login
 // nos va a devolver el responseEntity del controlador, un Json Web Token
 public class JwtDto {
     private String token;
-    private String bearer = "Bearer";
-    private String nombreUsuario;
-    private Collection<? extends GrantedAuthority> authorities;
+    
+    public JwtDto() {
+		
+	}
 
-    public JwtDto(String token, String nombreUsuario, Collection<? extends GrantedAuthority> authorities) {
+	public JwtDto(String token) {
         this.token = token;
-        this.nombreUsuario = nombreUsuario;
-        this.authorities = authorities;
     }
 
     public String getToken() {
@@ -25,27 +21,4 @@ public class JwtDto {
         this.token = token;
     }
 
-    public String getBearer() {
-        return bearer;
-    }
-
-    public void setBearer(String bearer) {
-        this.bearer = bearer;
-    }
-
-    public String getNombreUsuario() {
-        return nombreUsuario;
-    }
-
-    public void setNombreUsuario(String nombreUsuario) {
-        this.nombreUsuario = nombreUsuario;
-    }
-
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
-
-    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
-        this.authorities = authorities;
-    }
 }
