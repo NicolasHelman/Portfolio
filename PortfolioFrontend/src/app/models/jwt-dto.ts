46aaf1546e0a247dfa463ba87d44ee60c18f135d
Nicolas Helman <nicolashelman11@gmail.com>
2022-07-07 03:09:02 -0300

    Model JwtDto modified

--- PortfolioFrontend/src/app/models/jwt-dto.ts
+++ PortfolioFrontend/src/app/models/jwt-dto.ts
@@ -1,8 +1,9 @@
 export class JwtDto {
 
     token: string;
-    type: string;
-    nombreUsuario: string;
-    authorities: string[];
+
+    constructor(token: string) {
+        this.token = token;
+    }
     
 }
