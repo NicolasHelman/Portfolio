f6e176383b20024528a7d4b2494627226197594b
Nicolas Helman <nicolashelman11@gmail.com>
2022-07-20 14:19:14 -0300

    Controller Contacto modified

--- PortfolioBackend/src/main/java/com/PortfolioBackend/controllers/ContactoController.java
+++ PortfolioBackend/src/main/java/com/PortfolioBackend/controllers/ContactoController.java
@@ -7,6 +7,7 @@ import javax.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.PortfolioBackend.models.Contacto;
+import com.PortfolioBackend.security.models.Usuario;
+import com.PortfolioBackend.security.services.UsuarioService;
 import com.PortfolioBackend.services.ContactoService;
 
 @RestController
@@ -24,8 +27,17 @@ public class ContactoController {
 	@Autowired
 	private ContactoService contactoService;
 	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@PreAuthorize("hasRole('USER')")
 	@PostMapping("/save")
 	public ResponseEntity<Contacto> enviar(@RequestBody Contacto contacto) throws UnsupportedEncodingException, MessagingException {
+		
+		Usuario usuario = usuarioService.findBySesionUsuario();
+		
+		contacto.setUsuario(usuario);
+		
 		contactoService.save(contacto);
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(contacto);
